687e68868a5c92626d92f2c324d5cff89965b5a0
XhmikosR <xhmikosr@gmail.com>
2020-12-28 16:28:35 +0200

    Switch to GitHub Actions CI

--- build.bat
+++ build.bat
@@ -3,8 +3,11 @@ setlocal
 
 cd /d %~dp0
 
-rem add MSVC 64-bit in PATH
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+rem add MSVC in PATH
+call :SubVSPath
+if not exist "%VS_PATH%" echo ERROR: Visual Studio 2019 NOT FOUND! & goto end
+
+call "%VS_PATH%\Common7\Tools\vsdevcmd" -arch=amd64
 
 pushd CPP\7zip
 nmake /f makefile NEW_COMPILER=1 MY_STATIC_LINK=1 CPU=AMD64
@@ -12,5 +15,9 @@ popd
 
 :end
 endlocal
-rem pause
+if not defined CI pause
+exit /b
+
+:SubVSPath
+for /f "delims=" %%A in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64') do set "VS_PATH=%%A"
 exit /b
